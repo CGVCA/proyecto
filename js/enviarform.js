f86fcb922d06e7aa31e483ce3d1d0d76ad1cb5c3
CGVCA <gilberto.jimenez@uabc.edu.mx>
2025-02-28 20:40:46 -0800

    continuacion - proyecto

--- js/enviarform.js
+++ js/enviarform.js
@@ -5,12 +5,42 @@ function Enviarform(nombre, email, msg) {
     this.msg = msg;
 }
 
+//get yhe input from HTML
+const inputNombre = document.getElementById("txtNombre");
+const inputEmail = document.getElementById("txtEmail");
+const inputMsg = document.getElementById("txtMensaje");
+let p;
+
 function register() {
-    console.log("Registrando...");
+
+    //crear objeto
+    //desplejar el oj
+    let newEnviarform = new Enviarform(inputNombre.value, inputEmail.value, inputMsg.value);
+    console.log(newEnviarform);
+
+    if (inputNombre.value == "") {
+        alert("Ingrese el nombre")
+    } else {
+        display(newEnviarform);
+    }
+
+    //alert("Registro exitoso!")
+}
+
+function display(enviarform) {
+    const list = document.getElementById("list");
+    p = `
+        <div>
+            <p>${enviarform.nombre} - ${enviarform.email}</p>
+        </div>
+    `;
+
+    list.innerHTML += p; // insert lista
 }
 
 // objetos
 let enviarform1 = new Enviarform("Mario Jiménez Montes", "jimenez@uabc.edu.mx", "Tengo una duda con respecto a mi proceso de movilidad");
 let enviarform2 = new Enviarform("Juan Iván García", "ivan.garcia@uabc.edu.mx", "Si ya cuento con mi aceptación que proceso sigue a continuacioón.");
 
-console.log(enviarform1, enviarform2);
+display(enviarform1);
+display(enviarform2);
